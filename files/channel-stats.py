2a42b8e01ddf54bef622163be12496ac5ddbe779
jeffbrennan <jeffbrennan10@gmail.com>
2019-01-14 13:05:31 -0600

    add calculated columns, rename variables, simplified functions

--- files/channel-stats.py
+++ files/channel-stats.py
@@ -1,83 +1,93 @@
 import requests
 import json
-import csv
 import time
 import datetime
 import timing
 import pandas as pd
-import numpy as np
 
-keyGet = open('Key.txt', 'r')
-API_KEY = keyGet.read()
-timeNow = time.time()
-fileDate = datetime.datetime.fromtimestamp(timeNow).strftime('%m-%d--%H-%M--')
-idPath = 'C:/Users/jeffb/Documents/Python/webPrograms/webScraping/yt-stats/IDs/'
-outputPath = 'C:/Users/jeffb/Documents/Python/webPrograms/webScraping/yt-stats/output/'
+def json_parse(x):
+    try:
+        video_result = [x['id'], x['snippet']['publishedAt'],
+                        x['snippet']['channelTitle'], x['snippet']['title'],
+                        x['contentDetails']['duration'],
+                        x['statistics']['viewCount'],
+                        x['statistics']['likeCount'],
+                        x['statistics']['dislikeCount'],
+                        x['statistics']['commentCount']]
+    except KeyError:
+        print(x['id'] + ': key error')
+
+        video_result = [x['id'], x['snippet']['publishedAt'],
+                        x['snippet']['channelTitle'],
+                        x['snippet']['title'],
+                        x['contentDetails']['duration']]
+    return video_result
+
+def api_call(vid_query, results, API_KEY):
+    response = requests.get('https://www.googleapis.com/youtube/v3/videos?part=id%2C' +
+                            '+snippet%2C+contentDetails%2C+statistics' +
+                            '&id=' + vid_query + '&maxResults=' + str(results) +
+                            '&fields=items(contentDetails%2Fduration%2Cid%2Csnippet' +
+                            '(channelTitle%2CpublishedAt%2Ctitle)%2Cstatistics)&key=' + API_KEY)
+
+    vid_stats = json.loads(response.content)
+    response.close()
+    return vid_stats
+
+def df_transform(df):
+    num_columns = ['Likes', 'Dislikes', 'Comments', 'Views']
+    for x in num_columns:
+        df[x] = pd.to_numeric(df[x])
+
+    df['Engagement'] = (df['Likes'] + df['Dislikes'] + df['Comments']) / df['Views']
+    df['Like_Ratio'] = df['Likes'] / (df['Likes'] + df['Dislikes'])
+    df['Controversy'] = df['Comments'] / df['Views']
+
+    return df
 
 def stat_get(channel, request_type):
-    video_ids = pd.read_csv(idPath + channel + '-videoIDs.csv', squeeze=True).tolist()
-
     id_counter = -1
-    statPage_counter = 0
+    row_counter = 0
+
+    video_ids = pd.read_csv('vid_id/' + channel + '-videoIDs.csv', squeeze=True).tolist()
+    vid_df = pd.DataFrame(columns=['ID', 'Published', 'Channel', 'Video', 'Duration',
+                                   'Views', 'Likes', 'Dislikes', 'Comments'])
 
     if request_type == '2':
         video_ids = video_ids[0:25]
 
-    vidnum = len(video_ids)
-    pageTotal = (vidnum // 50) + 1
+    vid_num = len(video_ids)
+    page_num = (vid_num // 50) + 1
 
-    video_stats = pd.DataFrame(columns=['ID', 'Published_At', 'Channel Title',
-                                        'Video Title', 'Duration', 'View_Count',
-                                        'Like_Count', 'Dislike_Count', 'Comment_Count'])
-
-    for _ in range(pageTotal):
-        if (vidnum - id_counter) > 50:
-            video_request = '%2C+'.join(video_ids[id_counter + 1: id_counter + 51])
+    for page in range(page_num):
+        if (vid_num - id_counter) > 50:
+            vid_query = '%2C+'.join(video_ids[id_counter + 1: id_counter + 51])
             results = 50
-        elif (vidnum - id_counter) < 50:
-            video_request = '%2C+'.join(video_ids[id_counter + 1:])
+        elif (vid_num - id_counter) < 50:
+            vid_query = '%2C+'.join(video_ids[id_counter + 1:])
             results = len(video_ids[id_counter + 1:])
 
-        statGet = requests.get('https://www.googleapis.com/youtube/v3/videos?part=id%2C' +
-                               '+snippet%2C+contentDetails%2C+statistics' +
-                               '&id=' + video_request + '&maxResults=' + str(results) +
-                               '&fields=items(contentDetails%2Fduration%2Cid%2Csnippet' +
-                               '(channelTitle%2CpublishedAt%2Ctitle)%2Cstatistics)&key=' + API_KEY)
+        vid_stats = api_call(vid_query, results, API_KEY)
 
-        statInfo = json.loads(statGet.content)
-        statGet.close()
-
-        row_counter = 0
-        for x in statInfo['items']:
-            try:
-                video_result = [x['id'], x['snippet']['publishedAt'],
-                                x['snippet']['channelTitle'], x['snippet']['title'],
-                                x['contentDetails']['duration'],
-                                x['statistics']['viewCount'],
-                                x['statistics']['likeCount'],
-                                x['statistics']['dislikeCount'],
-                                x['statistics']['commentCount']]
-            except KeyError:
-                print(x['id'] + ': key error')
-
-                video_result = [x['id'], x['snippet']['publishedAt'],
-                                x['snippet']['channelTitle'],
-                                x['snippet']['title'],
-                                x['contentDetails']['duration']]
-                pass
-
-            video_stats.loc[row_counter] = video_result
+        for vid in vid_stats['items']:
+            video_result = json_parse(vid)
+            vid_df.loc[row_counter] = video_result
             row_counter += 1
 
-        id_counter = (id_counter + results)
-        statPage_counter += 1
-        print('Grabbing stats from ' + channel + ' (' + str(statPage_counter) + '/' + str(pageTotal) + ')')
+        id_counter += results
+        print('Grabbing stats from ' + channel + ' (' + str(page + 1) + '/' + str(page_num) + ')')
 
-    video_stats.to_csv(outputPath + fileDate + channel + '-stats.csv', encoding='utf8', index=False)
+    out_df = df_transform(vid_df)
+    out_df.to_csv('output/' + timestamp + channel + '-stats.csv', encoding='utf8', index=False)
 
 def stat_request():
-    channels = ['JRE']
+    channels = ['h3 podcast']
     request = str(input('1 - All statistics | 2 - Recent statistics: '))
-    [stat_get(i, request) for i in channels]
+    [stat_get(i, request) for i in channels]  # gets stats for every channel listed in 'channels'
+
+keyGet = open('Key.txt', 'r')
+API_KEY = keyGet.read()
+time_now = time.time()
+timestamp = datetime.datetime.fromtimestamp(time_now).strftime('%m-%d--%H-%M--')
 
 stat_request()
